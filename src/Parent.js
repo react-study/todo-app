3aecdde39416b9a65176051055ebbee9fcb70bc1
박민호 <pmh@bagminhoui-MacBook-Pro.local>
2016-10-09 20:32:15 +0900

    minho/03/01

---
+++ src/Parent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Child from './Child';
+
+class Parent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            people: [
+                {
+                    name: 'gomugom',
+                    phone: '010-1111-2222',
+                    show: false
+                },{
+                    name: 'iu',
+                    phone: '010-1133-3245',
+                    show: false
+                },{
+                    name: 'akmu',
+                    phone: '010-1122-3245',
+                    show: false
+                }
+            ]
+        }
+    }
+    handleClick(i){
+        console.log(this.state);
+        const newPeople = this.state.people;
+        newPeople[i].show = !newPeople[i].show;
+        this.setState({
+            people: newPeople
+        });
+    }
+    render(){
+        const people = this.state.people;
+        const children = people.map((v,i) => (
+            <Child
+                key = { 'person #' + i }
+                name = { v.name }
+                phone = { v.phone }
+                show = { v.show }
+                handleClick = {()=> this.handleClick(i)}
+            />
+        ));
+        return(
+            <ul>
+                {children}
+            </ul>
+        )
+    }
+}
+
+export default Parent;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Child from './Child'
+import Parent from './Parent';
+
 render(
-    <Child />
-    , document.getElementById('root')
-);
+    <Parent />,
+    document.getElementById('root')
+)
