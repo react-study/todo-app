332ebf40036cb1cebf5e8d93c062b61a448dcd72
bohaesoju <bohaesoju@gmail.com>
2016-10-15 10:08:16 +0900

    수정dd

--- src/Child.js
+++ src/Child.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 
 class Child extends React.Component {
     render () {
-        return <div>Hello!!<br/>HiHI</div>;
+        return <div>Hello World hello hello!!<br />HiHI</div>
     }
 }
 
