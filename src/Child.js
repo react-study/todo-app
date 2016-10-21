d0712cf077ac7e9357a41ab2a0a91f03db99e6b0
gomugom <power4ce@gmail.com>
2016-10-22 01:53:24 +0900

    todo app 실습 초기화

--- src/Child.js
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react'
-
-class Child extends React.Component {
-    render () {
-        return <div>Hello!!<br/>HiHI</div>;
-    }
-}
-
-export default Child;
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Child from './Child'
-render(
-    <Child />
-    , document.getElementById('root')
-);
