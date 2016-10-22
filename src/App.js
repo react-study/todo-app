dcadb595da8a246077282e9c5748f475ac80539b
bohaesoju <bohaesoju@gmail.com>
2016-10-22 12:43:46 +0900

    todo 추가/ 삭제 첫번째 커

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+const getUniqueId = ()=> Date.now();
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      todos : [
+        {id : 1000, text : '내귀에모기'},
+        {id : 1001, text : '내귀에도청장치'},
+        {id : 1002, text : '내귀에캔디'}
+      ]
+    }
+  }
+  addTodo(newTodo){
+    const newTodos = [...this.state.todos,
+      {id : getUniqueId(), text:newTodo}
+    ];
+    this.setState({todos:newTodos});
+  }
+  deleteTodo(id){
+    const newTodos = [...this.state.todos];
+    const targetIndex = newTodos.findIndex(v => v.id === id);
+    newTodos.splice(targetIndex, 1);
+    this.setState({ todos : newTodos });
+  }
+  render(){
+    return(
+      <div className = "todo-app">
+          <Header addTodo = {newTodo => this.addTodo(newTodo)} />
+          <TodoList todos = {this.state.todos}
+                    deleteTodo = {id => this.deleteTodo(id)}
+          />
+      </div>
+    )
+  }
+}
+
+export default App;
