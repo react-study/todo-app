ad03382f5bd82f40b5c926f16f1f725ac676d845
diffth <diffth@nate.com>
2016-10-22 12:45:27 +0900

    OhSeHeon/todo/01

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+
+    handleKeyDown(e){
+        const val = this._input.value;
+        if(!val || e.keyCode !== 13) return;
+
+        //
+        this.props.addTodo(val);
+        this._input.value = '';
+    }
+    render(){
+        return(
+
+            <header>
+                <h1 className="todo-app__header">todos</h1>
+                <input
+                    className="todo-app__new-todo"
+                    placeholder="What needs to be clone?"
+                    onKeyDown={e => this.handleKeyDown(e)}
+                    ref={ref => {this._input = ref;}}
+                />
+            </header>
+        )
+    };
+}
+
+export default Header;
---
+++ src/TodoList.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component{
+
+    render(){
+        const todoList = this.props.todos.map((v, i) => (
+            <Todo
+                key         ={i}
+                text        ={v.text}
+                done        ={v.done}
+                id          ={v.id}
+                deleteTodo  ={this.props.deleteTodo}
+            />
+        ));
+        return (
+            <div className="todo-app__main">
+                <div className="toggle-all" />
+                <ul className="todo-list">
+                    {todoList}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
