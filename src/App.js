ff2d29524590001f2a75efbb4abe81f5fd419aa7
WIN-7U3JQSUNRO4\Administrator <bohaesoju@gmail.com>
2016-10-28 10:51:09 +0900

    edit 커밋밋

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+const generateUId = ()=> Date.now();
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {id: 1000, text: '치킨에 맥주 한 잔'},
+                {id: 1001, text: '삼겹살에 소주 한 잔'},
+                {id: 1002, text: '리코타샐러드에 봉골레 파스타'},
+                {id: 1003, text: '떡순튀'}
+            ],
+            editing: null
+        };
+    }
+    handleAddTodo(text) {
+        this.setState({
+            todos: [ ...this.state.todos, {
+                id: generateUId(),
+                text
+            }]
+        });
+    }
+    handleDeleteTodo(id) {
+        const newTodos = [...this.state.todos];
+        const deleteIndex = newTodos.findIndex(v => v.id === id);
+        newTodos.splice(deleteIndex, 1);
+        this.setState({ todos: newTodos });
+    }
+    handleEditTodo(id) {
+        this.setState({
+            editing: id
+        });
+    }
+    handleSaveTodo(id, newText) {
+        const newTodos = [...this.state.todos];
+        const editIndex = newTodos.findIndex(v => v.id === id);
+        newTodos[editIndex].text = newText;
+        this.setState({
+            todos: newTodos,
+            editing: null
+        });
+    }
+    handleCancelEditTodo() {
+        this.setState({
+            editing: null
+        });
+    }
+    render() {
+        const {
+            todos,
+            editing
+        } = this.state;
+        return (
+            <div className="todo-app">
+                <Header handleAddTodo = {(text)=> this.handleAddTodo(text)} />
+                <TodoList
+                    todos={todos}
+                    editing={editing}
+                    handleEditTodo = {id=> this.handleEditTodo(id)}
+                    handleSaveTodo = {(id, newText)=> this.handleSaveTodo(id, newText)}
+                    handleCancelEditTodo = {()=> this.handleCancelEditTodo()}
+                    handleDeleteTodo = {id=> this.handleDeleteTodo(id)}
+                />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+const TodoList = ({
+    todos,
+    editing,
+    handleEditTodo,
+    handleSaveTodo,
+    handleCancelEditTodo,
+    handleDeleteTodo
+}) => {
+    const todoList = todos.map(({id, text, done}, i) => (
+        <Todo
+            key              = {id}
+            text             = {text}
+            done             = {done}
+            editing          = {editing === id}
+            onEditTodo       = {()=> handleEditTodo(id)}
+            onSaveTodo       = {text=> handleSaveTodo(id, text)}
+            onCancelEditTodo = {()=> handleCancelEditTodo()}
+            onDeleteTodo     = {()=> handleDeleteTodo(id)}
+        />
+    ));
+    return (
+        <div className="todo-app__main">
+            <div className="toggle-all" />
+            <ul className="todo-list">
+                {todoList}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
