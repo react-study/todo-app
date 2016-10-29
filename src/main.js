a63efcffa2c4f4373c9379df9e4505bad7623e25
gomugom <power4ce@gmail.com>
2016-10-29 15:11:32 +0900

    [Lecture] 05. TodoApp Router 적용

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './routerTest/App';
+import { Router, Route, browserHistory } from 'react-router'
+
+import App from './App';
 
 render(
-    <App />,
-    document.getElementById('root')
+    <Router history={browserHistory}>
+        <Route path="/(:filter)" component={App} />
+    </Router>
+    , document.getElementById('root')
 );
