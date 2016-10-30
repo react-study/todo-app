05197ed2e73cc6c6a49272f98461b4fb578bdf4a
perfectacle <perfectacle@naver.com>
2016-10-31 00:04:25 +0900

    원상 복구

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 
-const root = document.getElementById('root');
-ReactDOM.render(
+render(
   <App />,
-  root
+  document.getElementById('root')
 );
